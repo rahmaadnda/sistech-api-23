8f9e5cc19fec6c3673ef5de0fa9dd76373e677e6
nofamex <nsiren517@gmail.com>
2022-07-23 20:35:15 +0700

    feat/sentry

--- src/config.js
+++ src/config.js
@@ -5,17 +5,31 @@ const morgan = require("morgan");
 const tokenRouter = require("./routes/tokenRoutes");
 const blogRouter = require("./routes/blogRoutes");
 const rateLimit = require("express-rate-limit");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+require("dotenv").config();
 
 const app = express();
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
   message: "what u doin, stop spamming",
 });
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
@@ -26,5 +40,7 @@ app.use(limiter);
 
 app.use("/token", tokenRouter);
 app.use("/blog", blogRouter);
+
+app.use(Sentry.Handlers.errorHandler());
 
 module.exports = app;
