e6f8522c55e02e1444161e5f1aee3f8b44e7feda
zuhalal <zuhalalimulhadi19@gmail.com>
2023-07-01 12:06:16 +0700

    fix: env config

--- src/database/firestore.js
+++ src/database/firestore.js
@@ -1,4 +1,19 @@
-const firebaseConfig = require("../../firebase.json");
+require('dotenv').config()
+
+const firebaseConfig = {
+  "type": "service_account",
+  "project_id":  process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_id": process.env.CLIENT_ID,
+  "client_email": process.env.CLIENT_EMAIL,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": "googleapis.com"
+}
+
 const admin = require("firebase-admin");
 
 admin.initializeApp({ credential: admin.credential.cert(firebaseConfig) });
