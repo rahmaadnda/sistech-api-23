d7149e9e863270e790c9c0e35a21cbfca5250559
Rahma Adinda Putri <rahma.adinda@ui.ac.id>
2023-07-06 23:35:25 +0700

    fix deployment

--- src/database/firestore.js
+++ src/database/firestore.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
+require('dotenv').config({path: '.env.local'})
 
 const firebaseConfig = {
   "type": "service_account",
-  "project_id":  process.env.PROJECT_ID,
+  "project_id":  process.env.FIREBASE_PROJECT_ID,
   "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   "client_id": process.env.CLIENT_ID,
-  "client_email": process.env.CLIENT_EMAIL,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
